1ba9849afa6199ca2ed2e7abb8b02dd2025d4de1
rgtfekhem-code <rgtfekhem@gmail.com>
2025-09-07 18:24:26 +0300

    Create index.js

---
+++ pages/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">
+        My Portfolio 🚀
+      </h1>
+    </div>
+  )
+}
